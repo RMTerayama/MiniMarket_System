a02e4c8e1996923925bb8475110fa585fc8c5f32
Richard Masayuki Terayama <85762067+RMTerayama@users.noreply.github.com>
2022-04-20 16:16:33 -0400

    Update Information.java

--- src/Information.java
+++ src/Information.java
@@ -1,3 +1,14 @@
 public class Information {
-    
+    public class cliente{
+        String nome;
+        String cpf;
+        int ID;
+    } 
+    public class produto{
+        String produto;
+        int codigo;
+        double ValorDeCompra;
+        double ValorDeVenda;
+        int QtdEmEstoque;
+    }
 }
